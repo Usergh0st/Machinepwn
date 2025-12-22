4ad3519f59c5494c6625639ed273c960fd30e350
Usergh0st <usergh0stmail@proton.me>
2025-12-22 13:04:22 -0400

    feat: more fonts were added and the installer was improved somewhat (KaliInstaller.sh)

--- KaliInstaller.sh
+++ KaliInstaller.sh
@@ -17,40 +17,46 @@
 # Author: Enríque González Aka (Usergh0st)
 # Repository: https://github.com/Usergh0st/Machinepwn.git
 # Mail: usergh0stmail@proton.me
-# Last Update: 21.12.2025 8:54 AM
-# Description: Kali Linux installer script.
+# Last Update: 22.12.2025 8:54 AM
+# Script Version: 1.3
+# Coutings atempt: 5
 
 # Copyright (C) 2025-2026 Usergh0st <usergh0stmail@proton.me>
 # Copyright (C) 2026-2027 Usergh0st <usergh0stmail@proton.me>
 # Licensed under GPL-3.0 license
 # ==========================================================================
 
-# Colors use this script. | Colores en el script.
+# Colors use this script | Colores en el script
 Cyan="\e[1;36m"
 White="\e[1;37m"
 Blue="\e[1;34m"
 Reset="\e[0m"
 Green="\e[1;32m"
 LightRed="\e[1;31m"
+Bold="\e[1m"
 
-# Utils variables. | variables de utilidades.
+# Utils variables | variables de utilidades
 bspwm="https://github.com/baskerville/bspwm.git"
 sxhkd="https://github.com/baskerville/sxhkd.git"
 picom="https://github.com/yshui/picom.git"
 myrepo="https://github.com/Usergh0st/Machinepwn.git"
 
-# List of packages to install | lista de paquetes a instalar.
+# List of packages to install | lista de paquetes a instalar
 libs="libxcb-xkb-dev libxkbcommon-dev librsvg2-common build-essential libxcb1-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-keysyms1-dev \
       libxcb-xinerama0-dev libxcb-shape0-dev libxcb-cursor-dev pkg-config libxcb-icccm4-dev"
 
 xorg="xserver-xorg-core xserver-xorg-video-fbdev xserver-xorg-input-all x11-xserver-utils xinit xinput"
 
 pkgs="polybar rofi alacritty nitrogen zsh git wget curl net-tools xdotool pulseaudio-utils pulseaudio pavucontrol \
-     fastfetch papirus-icon-theme adwaita-icon-theme"
+     fastfetch papirus-icon-theme adwaita-icon-theme bat firefox-esr openvpn bleachbit mousepad feh"
 
-# Logo function | Funcion del logo.
+# Logo function | funcion del logo
 logo () {
-echo -e "${LightRed}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  export LC_ALL="${LC_ALL:-C.UTF-8}"
+  export LANG="${LANG:-C.UTF-8}"
+
+  printf '%b' "${Bold}${LightRed}"
+  cat <<'LOGO'
    ⠀⠀⠀⠀⠀⠀⠀⠄⠀⠰⡐⠀⠀⠀⠀⠀⠀⢀⣄⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⣿⡆⠀⠀⠀⠄⠀⠈⢀⣤⣤⣬⣥⣤⣀⡀⠀⠀⠀⠂⠀⠑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠂⠀⠀⠑⠀⠀⠙⠃⠵⠀⢀⣴⣿⡿⠿⠿⠿⠿⠿⠿⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠒⠀⠀⠀
@@ -71,17 +77,19 @@ echo -e "${LightRed}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
    ⢀⠀⠀⠶⠀⣿⣿⠿⣿⣿⣷⠦⠀⢸⠀⣿⣿⣿⣿⣿⡟⠉⠈⢿⣿⣿⣿⣿⣿⡇⡇⠀⢠⢾⠿⠶⣶⣿⣿⡄⢀⠀⠀⠀⠀
    ⠀⠀⠄⠀⠀⣿⣷⡏⠀⠀⠀⣰⡀⢻⠀⣿⣿⣿⣿⣿⣷⣄⣰⣿⣿⣿⣿⣿⣿⡇⡿⠀⣰⡀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀
    ⠀⠀⠀⠀⠀⠻⣿⣷⣀⣠⣴⣿⡿⢾⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⡷⠾⣿⣿⣶⣶⣿⣿⡿⠃⠀⠀⠀⠀⠀
-   ⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠹⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠗⠉⠉⠉⠉⠉⠉⠀  ⠀⠀ ${Reset}\n"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+   ⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠹⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠗⠉⠉⠉⠉⠉⠉⠀  ⠀⠀
+LOGO
+  printf '%b\n' "${Reset}"
 }
 
+# Fetch or traps ctrl_c | function para traquear el ctrl_c y salir.⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 trap ctrl_c INT
-
 function ctrl_c () {
 	echo -e "${LightRed} Exiting the script goodbye! ${Reset}\n"
 	exit 1
 }
 
-# Initial checks function | funcion de comprobaciones iniciales.
+# Initial checks function | funcion de comprobaciones iniciales
 initial_checks () {
 
 	# Get root temporary permissions | obtener permisos temporales de root.
@@ -118,6 +126,7 @@ initial_checks () {
 # Welcome function | funcion de bienvenida.
 welcome () {
 
+	# Welcome message | mensaje de bienvenida
 	clear ; logo
 
 	echo -e "${Cyan}This script will install my desktop environment and this is what it will do:${Reset}\n"
@@ -130,29 +139,34 @@ welcome () {
 	read
 }
 
+# Install dependencies function | funcion de instalacion de dependencias
 install_dependencies () {
 
-	# Install required packages and dependencies | instalar paquetes y dependencias necesarias.
+	# Install required packages and dependencies | instalar paquetes y dependencias necesarias
 	
 	clear ; logo
 
-	echo -e "${White}Updating and installing required packages and dependencies...${Reset}"
-	sudo apt update &>/dev/null ; sudo apt full-upgrade -y &>/dev/null ; sudo apt install -y ${libs} ${xorg} ${pkgs} --no-install-recommends &>/dev/null
-	echo -e "${Green}Once done the system was updated and the necessary dependencies were installed.${Reset}\n"
+	echo -e "${White}Updating and installing required packages and dependencies...${Reset}\n"
+	sudo apt update ; sudo apt full-upgrade -y ; sudo apt install -y ${libs} ${xorg} ${pkgs} --no-install-recommends
+
+	clear ; logo
+
+	echo -e "${Green}Once done the system was updated and the necessary dependencies were installed.${Reset}"
 }
 
+# Install bspwm, sxhkd and others function | funcion de instalacion de bspwm, sxhkd y otros
 install_bspwm_sxhkd_and_others () {
 
-	# Clone repositories | clonar repositorios.
+	# Clone repositories | clonar repositorios
 	echo -e "${White}Cloning repositories in the current working folder...${Reset}\n"
 
 		# Checks folder exist or no | comprobar si la carpeta existe o no.
-		if [ -f "${HOME}/cloning" ]; then
+		if [ -d "${HOME}/cloning" ]; then
 			echo -e "${White}The folder ${LightRed}(cloning)${White} already exists in your home directory.${Reset}\n"
 			rm -rf "${HOME}/cloning"
 		fi
 
-	mkdir -p cloning ; cd cloning ; git clone ${bspwm} &>/dev/null ; git clone ${sxhkd} &>/dev/null ; git clone ${picom} &>/dev/null ; git clone ${myrepo} &>/dev/null
+	mkdir -p cloning ; cd "cloning" ; git clone --depth 1 --no-tags ${bspwm} &>/dev/null ; git clone --depth 1 --no-tags ${sxhkd} &>/dev/null ; git clone --depth 1 --no-tags ${picom} &>/dev/null ; git clone --depth 1 --no-tags ${myrepo} &>/dev/null
 
 	# Install bspwm with repository | instalar bspwm con el repositorio.
 	echo -e "${White}Installing bspwm with the repository...${Reset}"
@@ -169,51 +183,85 @@ install_bspwm_sxhkd_and_others () {
 	# cd picom ; make ; sudo make install ; cd ..
 }
 
+# Backup old configurations function | funcion de backup de configuraciones antiguas
 Backup_old_configurations () {
-	# Backup old configurations | hacer backup de las configuraciones antiguas.
+	# Backup old configurations | hacer backup de las configuraciones antiguas
 	echo -e "${White}Backing up old configurations...${Reset}\n"
 
-	cd "${HOME}" ; mkdir -p "Backup" ; cd Backup
+	cd "${HOME}" ; mkdir -p "backup" ; cd "backup"
 
 	if [ -d "${HOME}/.config/bspwm" ]; then
-		mv "${HOME}/.config/bspwm" "${HOME}/Backup/bspwm.bak_$(date +%Y%m%d%H%M%S)"
+		mv "${HOME}/.config/bspwm" "${HOME}/backup/bspwm.bak_$(date +%Y%m%d%H%M%S)"
 		echo -e "${Green}bspwm configuration backed up.${Reset}" ; sleep 1.1
 	else
 		echo -e "${White}No existing bspwm configuration found skipping backup.${Reset}" ; sleep 1.1
 	fi
 
 	if [ -d "${HOME}/.config/sxhkd" ]; then
-		mv "${HOME}/.config/sxhkd" "${HOME}/Backup/sxhkd.bak_$(date +%Y%m%d%H%M%S)"
+		mv "${HOME}/.config/sxhkd" "${HOME}/backup/sxhkd.bak_$(date +%Y%m%d%H%M%S)"
 		echo -e "${Green}sxhkd configuration backed up.${Reset}" ; sleep 1.1
 	else
 		echo -e "${White}No existing sxhkd configuration found skipping backup.${Reset}" ; sleep 1.1
 	fi
 
 	if [ -d "${HOME}/.config/polybar" ]; then
-		mv "${HOME}/.config/polybar" "${HOME}/Backup/polybar.bak_$(date +%Y%m%d%H%M%S)"
+		mv "${HOME}/.config/polybar" "${HOME}/backup/polybar.bak_$(date +%Y%m%d%H%M%S)"
 		echo -e "${Green}polybar configuration backed up.${Reset}" ; sleep 1.1
 	else
 		echo -e "${White}No existing polybar configuration found skipping backup.${Reset}" ; sleep 1.1
 	fi
 
 	if [ -d "${HOME}/.config/alacritty" ]; then
-		mv "${HOME}/.config/alacritty" "${HOME}/Backup/alacritty.bak_$(date +%Y%m%d%H%M%S)"
+		mv "${HOME}/.config/alacritty" "${HOME}/backup/alacritty.bak_$(date +%Y%m%d%H%M%S)"
 		echo -e "${Green}alacritty configuration backed up.${Reset}" ; sleep 1.1
 	else
 		echo -e "${White}No existing alacritty configuration found skipping backup.${Reset}" ; sleep 1.1
 	fi
 
 	if [ -d "${HOME}/.config/picom" ]; then
-		mv "${HOME}/.config/picom" "${HOME}/Backup/picom.bak_$(date +%Y%m%d%H%M%S)"
+		mv "${HOME}/.config/picom" "${HOME}/backup/picom.bak_$(date +%Y%m%d%H%M%S)"
 		echo -e "${Green}picom configuration backed up.${Reset}" ; sleep 1.1
 	else
 		echo -e "${White}No existing picom configuration found skipping backup.${Reset}" ; sleep 1.1
 	fi
 }
 
-# Main routine | rutina principal.
+# Install machinepwn configuration files function | funcion de instalacion de archivos de configuracion de machinepwn
+install_machinepwn_configurations () {
+
+	# Copying directories also add permissions | copiando directorios y agregando permisos
+	echo -e "${White}Installing Machinepw configuration please wait...${Reset}" 
+	cd "${HOME}/cloning/Machinepwn/home/.config" ; cp -r * "${HOME}/.config"
+
+	# Copying directory bspwm .config | copiando directorio bspwm .config
+	cd "${HOME}/.config/bspwm/" ; chmod u+x bspwmrc ; chmod u+x sxhkdrc
+	cd "${HOME}/.config/bspwm/src" ; chmod u+x *
+	cd "${HOME}/.config/bspwm/barpwn" ; chmod u+x launch.sh
+	# cd "${HOME}/.config/bspwm/homebar" ; chmod u+x launch.sh
+	
+	# Copying zsh files and others | copiando archivos zsh y otras cosas
+	cd "${HOME}/cloning/Machinepwn/home/"
+	cp .xinitrc .zprofile "${HOME}"
+	cd "${HOME}/cloning/Machinepwn/debian"
+	cp .zshrc "${HOME}"
+
+	# Installing the fonts | instalar las fuentes
+	cd "${HOME}/cloning/Machinepwn/home/"
+	cp -r fonts "${HOME}/.local/share/fonts" ; fc-cache -fv &>/dev/null
+
+	# Installing others thins | instalar otras cosas.
+	folder="bin" ; mkdir -p "~/.local/${folder}"
+	cd "${HOME}/cloning/Machinepwn/misc" ; cp * "${HOME}/.local/${folder}"
+	chmod u+x * "${HOME}/.local/${folder}"
+	
+	echo -e "${Green}Machinepwn configuration installed correctly.${Reset}\n" ; sleep 1.1
+}
+
+# Main routine | rutina principal
 initial_checks
 welcome
+
 install_dependencies
 install_bspwm_sxhkd_and_others
 Backup_old_configurations
+install_machinepwn_configurations
